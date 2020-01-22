f20f374d1b25e08344c2ed505593a877ffb3fc45
Maya Mounzer <maeydelee14@yahoo.com>
2020-01-23 00:12:04 +0100

    Updates

---
+++ precache-manifest.346d6e703c73c2e9ac688fbe18c43e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5085b42296afe4feff6b14ff98b134d1",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "4b67ea3e2a85d74432e2",
+    "url": "/React/static/css/main.9a0e4db7.chunk.css"
+  },
+  {
+    "revision": "e2374992fe86c85be910",
+    "url": "/React/static/js/2.e8158035.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React/static/js/2.e8158035.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b67ea3e2a85d74432e2",
+    "url": "/React/static/js/main.0ae4a23f.chunk.js"
+  },
+  {
+    "revision": "5869549981d0c2c3473c",
+    "url": "/React/static/js/runtime-main.8632fba4.js"
+  }
+]);
